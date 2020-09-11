1ccdc97b06087d808ceee7d7d95a61dbb3e097ed
Ted Coleman <ted.p.coleman@gmail.com>
2020-09-11 14:10:52 -0500

    Encapsulate Clock

--- src/index.js
+++ src/index.js
@@ -2,21 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>
-}
-
-function App() {
-  return (
-    <div>
-      <Welcome name='Ted' />
-      <Welcome name='Jenny' />
-      <Welcome name='Addie' />
-      <Welcome name='Mila' />
-    </div>
-  );
-}
-
 class Clock extends React.Component {
   constructor(props) {
     super(props);
@@ -25,21 +10,33 @@ class Clock extends React.Component {
     };
   }
 
+  UNSAFE_componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(), 1000
+    );
+  }
+
+  UNSAFE_componentWillMount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>Clock class time: {this.state.date.toLocaleTimeString()}.</h2>
+        <h2>Encapsulated Clock: {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
 }
 
-function tick() {
-  ReactDOM.render(
-    <Clock />,
-    document.getElementById('root')
-  )
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+)
