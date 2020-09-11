fffb00f19914d0a8aae1eebdc0e30391b6bac12d
Ted Coleman <ted.p.coleman@gmail.com>
2020-09-11 15:38:34 -0500

    Toggle on/off button

--- src/index.js
+++ src/index.js
@@ -36,7 +36,32 @@ class Clock extends React.Component {
   }
 }
 
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(
+      state => ({
+        isToggleOn: !state.isToggleOn
+      })
+    );
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
 ReactDOM.render(
-  <Clock />,
+  <Toggle />,
   document.getElementById('root')
 )
