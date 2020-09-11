d23ad940a05c883fb44abf42f4c73a38adf7172d
Ted Coleman <ted.p.coleman@gmail.com>
2020-09-11 12:46:57 -0500

    Update with view function and class component

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Counting the time: </h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  )
+function ViewWelcome(props) {
+  return <h1>Hello, {props.name}</h1>
 }
 
-setInterval(tick, 1000);
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>
+  }
+}
+
+const viewElement = <ViewWelcome name="Addie" />;
+const element = <Welcome name="Mila" />;
+
+ReactDOM.render(
+  viewElement,
+  // element,
+  document.getElementById('root')
+)
