298470de36ccd7e573e8583fc814579437fb92cf
Ted Coleman <ted.p.coleman@gmail.com>
2020-09-11 13:53:48 -0500

    Update clock to class, but more changes needed

--- src/index.js
+++ src/index.js
@@ -6,9 +6,40 @@ function Welcome(props) {
   return <h1>Hello, {props.name}</h1>
 }
 
-const element = <Welcome name="Mila" />;
+function App() {
+  return (
+    <div>
+      <Welcome name='Ted' />
+      <Welcome name='Jenny' />
+      <Welcome name='Addie' />
+      <Welcome name='Mila' />
+    </div>
+  );
+}
 
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-)
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>Clock class time: {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+function tick() {
+  ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+  )
+}
+
+setInterval(tick, 1000);
