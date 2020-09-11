0a6f40668e6099b86364a372a06f86e0bfdff4c7
Ted Coleman <ted.p.coleman@gmail.com>
2020-09-11 17:25:42 -0500

    Create NumberList with key

--- src/index.js
+++ src/index.js
@@ -2,69 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: new Date(),
-    };
-  }
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>{number}</li>
+  );
 
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(), 1000
-    );
-  }
-
-  componentWillMount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date(),
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Encapsulated Clock</h1>
-        <h2>{this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
+  return (
+    <ul>{listItems}</ul>
+  );
 }
 
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isToggleOn: true };
-
-    // Need to bind to onClick in the constructor
-    // this.handleClick = this.handleClick.bind(this);
-  }
-
-  // Arrow function is experiemntal public class method
-  handleClick = () => {
-  // handleClick() {
-    this.setState(
-      state => ({
-        isToggleOn: !state.isToggleOn
-      })
-    );
-  }
-
-  render() {
-    return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? 'ON' : 'OFF'}
-      </button>
-    );
-  }
-}
-
+const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
-  <Toggle />,
+  <NumberList numbers={numbers} />,
   document.getElementById('root')
 )
