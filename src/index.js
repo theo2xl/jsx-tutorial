0af43225eefe1ae5c620fef5fd94cf09c35211bf
Ted Coleman <ted.p.coleman@gmail.com>
2020-09-11 12:27:16 -0500

    Render DOM from callback timer

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Counting the time: </h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  )
 }
 
-function printFirstName(user) {
-  return user.firstName;
-}
-
-const user = {
-  firstName: 'Addie',
-  lastName: 'Coleman'
-}
-
-const element = <h1>Hi, {printFirstName(user)}</h1>;
-
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-)
+setInterval(tick, 1000);
