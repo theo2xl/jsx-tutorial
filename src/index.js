a75127edcef07d4e9c49576848d27f740799406d
Ted Coleman <ted.p.coleman@gmail.com>
2020-09-11 12:48:29 -0500

    Only use the view function

--- src/index.js
+++ src/index.js
@@ -2,21 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function ViewWelcome(props) {
+function Welcome(props) {
   return <h1>Hello, {props.name}</h1>
 }
 
-class Welcome extends React.Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>
-  }
-}
-
-const viewElement = <ViewWelcome name="Addie" />;
 const element = <Welcome name="Mila" />;
 
 ReactDOM.render(
-  viewElement,
-  // element,
+  element,
   document.getElementById('root')
 )
