ce7204eeb83874f6d8968e1f007c87b25049fb4b
Ted Coleman <ted.p.coleman@gmail.com>
2020-09-11 12:13:00 -0500

    Add functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
 
-const name = 'Ted Coleman';
-const element = <h1>Hello, {name}</h1>;
+function printFirstName(user) {
+  return user.firstName;
+}
+
+const user = {
+  firstName: 'Addie',
+  lastName: 'Coleman'
+}
+
+const element = <h1>Hi, {printFirstName(user)}</h1>;
 
 ReactDOM.render(
   element,
