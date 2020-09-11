4e2d9401337e3efc9c3158cd5f05b9f705fa9b31
Ted Coleman <ted.p.coleman@gmail.com>
2020-09-11 17:33:26 -0500

    Add embedded JSX expression to NumberList

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 
 function NumberList(props) {
   const numbers = props.numbers;
-  const listItems = numbers.map((number) =>
-    <li key={number.toString()}>{number}</li>
-  );
 
   return (
-    <ul>{listItems}</ul>
+    <ul>
+      {numbers.map((number) =>
+        <li key={number.toString()}>{number}</li>
+      )}
+    </ul>
   );
 }
 
