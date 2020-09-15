998dc7620d79ddc2ad941171ff01dedb639e039d
Ted Coleman <ted.p.coleman@gmail.com>
2020-09-15 10:39:56 -0500

    Add HelloWorld

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HelloWorld from './HelloWorld';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <HelloWorld name='Ted' />
   </React.StrictMode>,
   document.getElementById('root')
 );
